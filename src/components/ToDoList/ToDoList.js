6b244ec39555f327b9d3e51096fe0c12c78ebc46
Leon Sandvoss <leonsandvoss@Leons-MacBook-2.fritz.box>
2020-07-08 19:30:39 +0200

    first commit

---
+++ src/components/ToDoList/ToDoList.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ToDoItem from "../ToDoItem/ToDoItem";
+
+export default class ToDoList extends Component {
+  render() {
+    return <section></section>;
+  }
+}
